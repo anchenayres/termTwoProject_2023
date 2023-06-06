360e5f048c313d1d0b533fe7fc0a0838241a271f
anchenayres <21100284@virtualwindow.co.za>
2023-06-06 21:44:23 +0200

    Firebase Added

--- App.js
+++ App.js
@@ -4,17 +4,29 @@ import LoginScreen from './screens/LoginScreen'
 import RegisterSceen from './screens/RegisterScreen'
 import ProjectScreen from './screens/ProjectScreen'
 import ProfileScreen from './screens/ProfileScreen'
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 export default function App() {
+
+  const Drawer = createDrawerNavigator();
 
   //TODO: setup navigation here
   //TODO: check if user is logged in
 
   return (
+
+    // <NavigationContainer>
+    //   <Drawer.Navigator initialRouteName="LoginScreen" component={LoginScreen}>
+    //     <Drawer.Screen name='Profile' component={ProfileScreen}/>
+    //     <Drawer.Screen name='Project' component={ProjectScreen}/>
+    //   </Drawer.Navigator>
+    // </NavigationContainer>
+
     <SafeAreaView>
-      <StatusBar />
-      <ProfileScreen />
-      {/* <LoginScreen/> */}
+      {/* <StatusBar />
+      <ProfileScreen /> */}
+      <LoginScreen/>
     </SafeAreaView>
   );
 }
---
+++ utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDdwUULobxcvRZ3mqIdMgamvaugOdeiDMk",
+  authDomain: "termtwoproject-971ca.firebaseapp.com",
+  projectId: "termtwoproject-971ca",
+  storageBucket: "termtwoproject-971ca.appspot.com",
+  messagingSenderId: "128512827806",
+  appId: "1:128512827806:web:c92ed89c440356e5cef68f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
